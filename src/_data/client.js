ffb9ccd22bd3e209fdf1793cfa306c6bbb21619a
Paul Nicholsen <paulnicholsen27@gmail.com>
2026-07-29 14:16:17 -0400

    added social links

--- src/_data/client.js
+++ src/_data/client.js
@@ -13,8 +13,8 @@ module.exports = {
         mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
     },
     socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+		facebook: "https://www.facebook.com/profile.php?id=61590554107424",
+		instagram: "https://www.instagram.com/bonnilynkuhn/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     domain: "https://www.dragonquillbooks.com",
